e7121afc2b0c3382145ba8c859575e7fa9a007f9
Luiz Gabriel <luizgabriel1233@gmail.com>
2024-05-12 13:14:13 -0300

    chore: instalação do cleark e finalização da página de marketing

---
+++ components/logo.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+import localFont from 'next/font/local'
+import { cn } from '@/lib/utils'
+
+const headingFont = localFont({
+    src: '../public/fonts/font.woff2'
+})
+
+const Logo = () => {
+    return (
+        <Link href="/">
+            <div className='hover:opacity-75 transition items-center gap-x-2 hiddne md:flex'>
+                <Image
+                    src="/logo.png"
+                    alt='Logo'
+                    height={30}
+                    width={30}
+                />
+                <p className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}>Taskflow</p>
+            </div>
+        </Link>
+    )
+}
+
+export default Logo
